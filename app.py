09bdb47e6ac91c949e208b71e0a2de8a2f939e96
toddsierens <todd.sierens@shopify.com>
2019-04-15 23:02:36 -0400

    new host

--- app.py
+++ app.py
@@ -47,4 +47,4 @@ def handle_webhook():
 if __name__ == "__main__":
     port = os.environ.get('PORT', 5000)
     print "app is running on port {}".format(port)
-    app.run(port = port)
+    app.run(host='0.0.0.0', port=port)
