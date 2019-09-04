71ee384044013e8ab133776466cfc88a3086f0ca
toddsierens <todd.sierens@shopify.com>
2019-09-04 13:28:26 -0400

    player class and push notifications

---
+++ player.py
@@ -0,0 +1,23 @@
+import requests
+
+GAME = "value1"
+NAME = "value2"
+TURN = "value3"
+UNKNOWN = "unknown"
+DISCORD_MESSAGE_FORMAT = "{}, you're up! Turn number {} in game {}!"
+PUSH_FORMAT = "You're up! Turn number {} in game {}!"
+
+class Player(object):
+    def __init__(self, discord_name = None, discord_webhooks = None, push_notification = None):
+        self.discord_name = discord_name or ""
+        self.discord_webhooks = discord_servers or []
+        self.push_notification = push_notification or ""
+        
+    
+    def send(self, turn, game_name):
+        s = DISCORD_MESSAGE_FORMAT.format(self.discord_name, turn, game_name)
+        for webhook in self.discord_webhooks:
+            requests.post(webhook, json = {"content": s})
+        s = message_format.format(self.name, turn, game_name)
+        if self.push_notification:
+            requests.post(self.push_notification, data = s)
